55fd8ee4428393a9526285c514bdd2094d0bd726
pnagakoushik <pnagakoushik@gmail.com>
2025-02-03 14:18:20 +0530

    Update elibrarymanagement.c

--- elibrarymanagement.c
+++ elibrarymanagement.c
@@ -2,74 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct library {
-	char book_name[20];
-	char author[20];
-	int pages;
-	float price;
+struct Book {
+	char title[20];
+	char writer[20];
+	int total_pages;
+	float cost;
 };
+
 int main()
 {
-	struct library lib[100];
-	char ar_nm[30], bk_nm[30];
-	int i, input, count;
-	i = input = count = 0;
+	struct Book collection[100];
+	char writer_name[30], title_name[30];
+	int i, choice, book_count;
+	i = choice = book_count = 0;
 
-	while (input != 5) {
+	while (choice != 5) {
 
-		printf("\n\n******WELCOME TO E-LIBRARY*******\n");
-		printf("\n 1. Add book information\n 2. Displaybook information\n");
-		printf("3. List all books of given author\n");
-		printf("\n 4. List the count of books in the library\n");
+		printf("\n\n******WELCOME TO DIGITAL LIBRARY*******\n");
+		printf("\n 1. Add book details\n 2. Display book details\n");
+		printf("3. List all books by a given writer\n");
+		printf("\n 4. Show the total number of books in the library\n");
 		printf("5. Exit");
 
-		printf("\n Enter one of the above: ");
-		scanf("%d", &input);
+		printf("\n Enter your choice: ");
+		scanf("%d", &choice);
 
-		switch (input) 
+		switch (choice) 
 		{
 		case 1:
-			printf("Enter book name = ");
-			scanf("%s", lib[i].book_name);
+			printf("Enter book title = ");
+			scanf("%s", collection[i].title);
 
-			printf("Enter author name = ");
-			scanf("%s", lib[i].author);
+			printf("Enter writer name = ");
+			scanf("%s", collection[i].writer);
 
-			printf("Enter pages = ");
-			scanf("%d", &lib[i].pages);
+			printf("Enter total pages = ");
+			scanf("%d", &collection[i].total_pages);
 
-			printf("Enter price = ");
-			scanf("%f", &lib[i].price);
-			count++;
+			printf("Enter cost = ");
+			scanf("%f", &collection[i].cost);
+			book_count++;
 
 			break;
 
 		case 2:
-			printf("you have entered the following information \n");
-			for (i = 0; i < count; i++) 
+			printf("You have entered the following details:\n");
+			for (i = 0; i < book_count; i++) 
 			{
-				printf("book name = %s",lib[i].book_name);
-
-				printf("\t author name = %s",lib[i].author);
-
-				printf("\t pages = %d",lib[i].pages);
-
-				printf("\t price = %f",lib[i].price);
+				printf("Title = %s", collection[i].title);
+				printf("\t Writer = %s", collection[i].writer);
+				printf("\t Pages = %d", collection[i].total_pages);
+				printf("\t Cost = %.2f", collection[i].cost);
 			}
 			break;
 
 		case 3:
-			printf("Enter author name : ");
-			scanf("%s", ar_nm);
-			for (i = 0; i < count; i++) 
+			printf("Enter writer name: ");
+			scanf("%s", writer_name);
+			for (i = 0; i < book_count; i++) 
 			{
-				if (strcmp(ar_nm,lib[i].author)== 0)
-					printf("%s%s%d%f",lib[i].book_name,lib[i].author,lib[i].pages,lib[i].price);
+				if (strcmp(writer_name, collection[i].writer) == 0)
+					printf("%s %s %d %.2f", collection[i].title, collection[i].writer, collection[i].total_pages, collection[i].cost);
 			}
 			break;
 
 		case 4:
-			printf("\n No of books in library : %d",count);
+			printf("\n Total number of books in library: %d", book_count);
 			break;
 		case 5:
 			exit(0);
